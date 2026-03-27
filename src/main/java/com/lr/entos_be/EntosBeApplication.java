72b2b9f8393c77bf2c4727c7846c1d9d7bcf5a64
LUCIA <khinezarthwin21st@gmail.com>
2026-03-27 23:47:35 +0630

    Folder Structure

---
+++ src/main/java/com/lr/entos_be/EntosBeApplication.java
@@ -0,0 +1,13 @@
+package com.lr.entos_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntosBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntosBeApplication.class, args);
+	}
+
+}
